ae40168f76538658d50cf0c1b9d4aa22fae456c9
IsanFaizun <113765539+IsanFaizun@users.noreply.github.com>
2023-06-07 12:17:59 +0700

    First Commit

---
+++ src/Game.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Game extends JFrame {
+    public Game(){
+        setTitle("Othello");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+    }
+}
---
+++ src/GamePanel.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+import java.awt.event.MouseListener;
+
+public class GamePanel extends JPanel implements MouseListener {
+    public enum
+}
